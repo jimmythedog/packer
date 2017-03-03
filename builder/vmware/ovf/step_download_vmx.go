0c99eeacfd94a8e68876ac169e09e93dedff9ecd
Kevin Gibbs <kgibbs@cafex.com>
2017-03-03 09:44:36 +0000

    Use temp dir for downloaded vmx

--- builder/vmware/ovf/step_download_vmx.go
+++ builder/vmware/ovf/step_download_vmx.go
@@ -5,6 +5,7 @@ import (
 	"github.com/mitchellh/multistep"
 	vmwcommon "github.com/mitchellh/packer/builder/vmware/common"
 	"github.com/mitchellh/packer/packer"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -21,11 +22,11 @@ import (
 //	 <nothing>
 type StepDownloadVMX struct {
 	RemoteType string
-	vmxPath    string
+	tempDir    string
 }
 
 func (s *StepDownloadVMX) Run(state multistep.StateBag) multistep.StepAction {
-	log.Printf("StepDownloadVMX.Run entry: RemoteType=%s, vmxPath=%s", s.RemoteType, s.vmxPath)
+	log.Printf("StepDownloadVMX.Run entry: RemoteType=%s", s.RemoteType)
 
 	if s.RemoteType != "esx5" {
 		log.Printf("Will not download VMX due to incorrect remote type (%s)", s.RemoteType)
@@ -39,21 +40,24 @@ func (s *StepDownloadVMX) Run(state multistep.StateBag) multistep.StepAction {
 		return multistep.ActionHalt
 	}
 
-	s.vmxPath = state.Get("vmx_path").(string)
-	ui := state.Get("ui").(packer.Ui)
-	ui.Say(fmt.Sprintf("Downloading VMX from: %s to: %s", s.vmxPath, s.vmxPath))
-
-	if err := os.MkdirAll(filepath.Dir(s.vmxPath), 0700); err != nil {
-		state.Put("error", fmt.Errorf("Error making directory for local VMX: %s", err))
+	remoteVmxPath := state.Get("vmx_path").(string)
+	tempDir, err := ioutil.TempDir("", "packer-vmx")
+	if err != nil {
+		state.Put("error", fmt.Errorf("Error creating temp dir (%s): %s", tempDir, err))
 		return multistep.ActionHalt
 	}
+	s.tempDir = tempDir
+	vmxPath := filepath.Join(s.tempDir, filepath.Base(remoteVmxPath))
 
-	if err := remoteDriver.Download(s.vmxPath, s.vmxPath); err != nil {
+	ui := state.Get("ui").(packer.Ui)
+	ui.Say(fmt.Sprintf("Downloading VMX from: %s to: %s", remoteVmxPath, vmxPath))
+
+	if err = remoteDriver.Download(remoteVmxPath, vmxPath); err != nil {
 		state.Put("error", fmt.Errorf("Error writing VMX: %s", err))
 		return multistep.ActionHalt
 	}
-	ui.Say(fmt.Sprintf("VMX downloaded to: %s", s.vmxPath))
-	state.Put("vmx_path", s.vmxPath)
+	ui.Say(fmt.Sprintf("VMX downloaded to: %s", vmxPath))
+	state.Put("vmx_path", vmxPath)
 	log.Printf("StepDownloadVMX.Run exit")
 
 	return multistep.ActionContinue
@@ -61,14 +65,10 @@ func (s *StepDownloadVMX) Run(state multistep.StateBag) multistep.StepAction {
 
 func (s *StepDownloadVMX) Cleanup(multistep.StateBag) {
 	log.Printf("StepDownloadVMX.Cleanup entry")
-	if s.vmxPath != "" {
-		log.Printf("Deleting %s", s.vmxPath)
-		if err := os.Remove(s.vmxPath); err != nil {
-			log.Printf("Failed to remove local VMX file(%s): %s", s.vmxPath, err)
-		}
-		vmxDir := filepath.Dir(s.vmxPath)
-		if err := os.Remove(vmxDir); err != nil {
-			log.Printf("Failed to remove local VMX dir(%s): %s", vmxDir, err)
+	if s.tempDir != "" {
+		log.Printf("Deleting %s", s.tempDir)
+		if err := os.RemoveAll(s.tempDir); err != nil {
+			log.Printf("Failed to remove local VMX dir(%s): %s", s.tempDir, err)
 		}
 	}
 	log.Printf("StepDownloadVMX.Cleanup exit")
