c72b762b1639c0ca9da77724bb8263ebdad8e90b
Jimmy The Dog <github@kgibbs.co.uk>
2017-03-13 08:15:39 +0000

    Lookup ovftool on darwin

--- builder/vmware/common/driver_ovftool_unix.go
+++ builder/vmware/common/driver_ovftool_unix.go
@@ -4,9 +4,22 @@
 package common
 
 import (
+	"log"
 	"os/exec"
+	"runtime"
 )
 
 func ovftoolFindOvfTool() (string, error) {
-	return exec.LookPath("ovftool")
+	log.Printf("ovftoolFindOvfTool ENTRY")
+	path, err := exec.LookPath("ovftool")
+	if err != nil {
+		if runtime.GOOS == "darwin" {
+			appPath := "/Applications/VMware OVF Tool/ovftool"
+			log.Printf("Attempting to lookup ovftool in %s", appPath)
+			path, err = exec.LookPath(appPath)
+		}
+	}
+
+	log.Printf("ovftoolFindOvfTool EXIT: path=%s, err=%s", path, err)
+	return path, err
 }
